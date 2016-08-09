449efa86b8c90ed1cf3a37aa6cbf276a7366401f
tiger <wanhu88@gmail.com>
2016-08-09 12:31:23 +0800

    up

--- src/routes/middleware-list.js
+++ src/routes/middleware-list.js
@@ -10,88 +10,85 @@ router.get('/middlewares', function(req, res) {
             // 描述
             des: '根据请求路径，解析资源类型',
             npm: 'https://www.npmjs.com/package/astros-asset-parse',
-            version: 1,
-            tags: 'astros、映射、解析'.split('、'),
+            version:'1.0.2',
+            tags: '映射、解析'.split('、'),
             //热度
             hot: 2589
-                // 作者
-                // 相关中间件
-                // 更新时间
         }, {
             name: 'astros-resource-refer',
             npm: 'https://www.npmjs.com/package/astros-resource-refer',
-            version: 1,
-            tags: 'astros、资源引用'.split('、'),
+            version:'1.0.7',
+            tags: '资源引用'.split('、'),
             hot: 2589,
             des: ' 分析依赖资源'
         }, {
             name: 'astros-2ximg',
             npm: 'https://www.npmjs.com/package/astros-2ximg',
-            version: 1,
-            tags: 'astros、图片、二倍图'.split('、'),
+            version:'1.0.3',
+            tags: '图片、二倍图'.split('、'),
             hot: 2589,
             des: '图片'
         }, {
             name: 'astros-webcom-refer',
             npm: 'https://www.npmjs.com/package/astros-webcom-refer',
-            version: 1,
-            tags: 'astros、web组件'.split('、'),
+            version:'1.0.0',
+            tags: 'web组件'.split('、'),
             hot: 2589,
             des: '    '
         }, {
             name: 'astros-cmd-dep',
             npm: 'https://www.npmjs.com/package/astros-cmd-dep',
-            version: 1,
-            tags: 'astros、CMD、同步加载'.split('、'),
+            version:'1.0.3',
+            tags: 'CMD、同步加载'.split('、'),
             hot: 2589,
             des: ' '
         }, {
             name: 'astros-cmd-read',
             npm: 'https://www.npmjs.com/package/astros-cmd-read',
-            version: 1,
-            tags: 'astros、CMD、同步加载'.split('、'),
+            version:'1.0.1',
+            tags: 'CMD、同步加载'.split('、'),
             hot: 2589,
             des: ' '
         }, {
             name: 'astros-js-dep',
             npm: 'https://www.npmjs.com/package/astros-js-dep',
-            version: 1,
-            tags: 'astros、JS、依赖'.split('、'),
+            version:'1.0.2',
+            tags: 'JS、依赖'.split('、'),
             hot: 2589,
             des: '    '
         }, {
             name: 'astros-js-process',
             npm: 'https://www.npmjs.com/package/astros-js-process',
-            version: 1,
-            tags: 'astros、JS、合并'.split('、'),
+            version:'1.0.1',
+            tags: 'JS、合并'.split('、'),
             hot: 2589,
             des: '    '
         }, {
             name: 'astros-cmd-define',
             npm: 'https://www.npmjs.com/package/astros-cmd-define',
-            version: 1,
-            tags: 'astros、CMD、同步加载'.split('、'),
+            version:'1.0.2',
+            tags: 'CMD、同步加载'.split('、'),
             hot: 2589,
             des: ' '
         }, {
             name: 'astros-js-minify',
             npm: 'https://www.npmjs.com/package/astros-js-minify',
-            version: 1,
-            tags: 'astros、JS、压缩、混淆'.split('、'),
+            version:'1.0.4',
+            tags: 'JS、压缩、混淆'.split('、'),
             hot: 2589,
             des: ' '
         }, {
             name: 'astros-css-less2',
             npm: 'https://www.npmjs.com/package/astros-css-less2',
-            version: 1,
-            tags: 'astros、样式、LESS'.split('、'),
+            version:'1.0.3',
+            tags: '样式、LESS'.split('、'),
             hot: 2589,
             des: '  '
         }, {
             name: 'astros-css-sprite',
             npm: 'https://www.npmjs.com/package/astros-css-sprite',
-            version: 1,
-            tags: 'astros、图片、雪碧图'.split('、'),
+            version:'1.0.3',
+            tags: '图片、雪碧图'.split('、'),
             hot: 2589,
             des: '   '
         }]
